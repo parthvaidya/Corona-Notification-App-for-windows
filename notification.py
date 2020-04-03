42ee8b079269a6cd7edb636fb82b346259e5238e
sroyalgupta <sroyalgupta@gmail.com>
2020-04-03 12:47:50 +0530

    initial commit

---
+++ notification.py
@@ -0,0 +1,43 @@
+from plyer import notification
+import requests
+from bs4 import BeautifulSoup
+import time
+
+def notifyMe(title,message):
+    notification.notify(
+        title = title,
+        message = message,
+        app_icon = r"D:\Notification Project\icon.ico",
+        timeout = 30
+    )
+
+def getData(url):
+    r = requests.get(url)
+    return r.text
+
+# notifyMe("sagar","Hello")
+while True:
+    myHtmlData = getData("https://www.mohfw.gov.in/")
+    data =""
+    soup = BeautifulSoup(myHtmlData, 'html.parser')
+    for tr in soup.find_all('tbody'):
+        data += tr.get_text()
+    data_list = data.split("\n\n")
+
+    states = ['Maharashtra']
+    nested_list = []
+    for item in data_list[0:31]:
+        final_list = item.split("\n")[1:]
+        nested_list.append(final_list)
+    temp = data_list[31].split("\n")
+
+    for i in nested_list[1:]:
+        if i[1] in states:
+            title = "Coronovirus cases : "
+            text = f"State : {i[1]}\nTotal Cases : {i[2]}\nTotal Cured : {i[3]} \nTotal Deaths : {i[4]}\n"
+            notifyMe(title,text)
+            time.sleep(5)
+            title = "Total Number of cases in India : "
+            text = f"{temp[2]}\n\nMade By Sagar"
+            notifyMe(title,text)
+    time.sleep(1800)
---
+++ setup_notification.py
@@ -0,0 +1,3 @@
+from cx_Freeze import setup,Executable
+setup(name="Corona Notification Software",version= "0.1",description="Notification App",
+executables = [Executable(r"D:\Notification Project\notification.py")])
